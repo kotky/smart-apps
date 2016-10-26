37bd4b4d58d6a1b33fc5a8290a0ea571702e7bc0
Code Monkey <codemonkey@Codes-MacBook-Pro.local>
2016-10-26 15:55:35 +0200

    new statics, added test lib

--- multimeter/controllers.py
+++ multimeter/controllers.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from rest_framework.renderers import JSONRenderer
+from random import randint
 
 class JSONResponse(HttpResponse):
     """
@@ -9,3 +10,6 @@ class JSONResponse(HttpResponse):
         content = JSONRenderer().render(data)
         kwargs['content_type'] = 'application/json'
         super(JSONResponse, self).__init__(content, **kwargs)
+
+def GenerateRandomInRange(start, end):
+	return randint(start,end)
