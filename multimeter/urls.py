f0c89cc105a5d118632efc9a3275442f5ed1f9bf
Code Monkey <codemonkey@Codes-MacBook-Pro.local>
2016-10-27 15:14:33 +0200

    added canvas model binding

--- multimeter/urls.py
+++ multimeter/urls.py
@@ -4,5 +4,5 @@ from multimeter import views
 urlpatterns = [
     url(r'^api/(?P<pk>[0-9]+)', views.universal_json_container_detail),
     url(r'^api', views.universal_json_container_list),
-    url(r'^', views.index),
+    url(r'^$', views.index),
 ]
